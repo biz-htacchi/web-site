f95640ef3450b69ae415fc129d76d2304efe2658
biz-htacchi <div.htacchi67@gmail.com>
2025-07-04 14:20:38 +0900

    TOP：完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,24 @@
 //==============================================//
-// 
+// ハンバーガーメニュー //
 //==============================================//
+var hamburger = $('.header__hamburger-menu');
+
+// OPEN/CLOSEボタンをクリックしたら
+$('.header__hamburger-button').on('click', function () {
+  // .hamburgerの表示・非表示を繰り返す
+  hamburger.toggleClass('header__hamburger-menu-active');
+});
+
+// アンカーリンクをクリックしたら閉じる
+$('.header__header-btn').on('click', function () {
+  // ハンバーガーメニューを閉じる
+  hamburger.removeClass('header__hamburger-menu-active');
+});
+
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+  // ハンバーガーメニューを閉じる
+  hamburger.removeClass('header__hamburger-menu-active');
+});
 
 
